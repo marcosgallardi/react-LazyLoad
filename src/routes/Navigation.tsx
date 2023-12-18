4c5157346d9eff85aa210e82fd6aae20a2cf7013
marcosgallardi <gallardimarcos98@gmail.com>
2023-12-18 18:18:43 -0300

    avanzando en el lazyload

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,36 +1,45 @@
-import {
-  BrowserRouter ,
-  Routes,
-  Route,
-  NavLink
-} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 
-import logo from '../logo.svg';
+import logo from "../logo.svg";
+import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages";
 
 export const Navigation = () => {
   return (
     <BrowserRouter>
       <div className="main-layout">
         <nav>
-            <img src={ logo } alt="React Logo" />
+          <img src={logo} alt="React Logo" />
           <ul>
             <li>
-              <NavLink to="/lazy1" activeClassName="nav-active" exact>Home</NavLink>
+              <NavLink
+                to="/lazy1"
+                className={({ isActive }) => (isActive ? "nav-active" : "")}>
+                Lazy 1
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/lazy2" activeClassName="nav-active" exact>About</NavLink>
+              <NavLink
+                to="/lazy2"
+                className={({ isActive }) => (isActive ? "nav-active" : "")}>
+                Lazy 2
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/lazy3" activeClassName="nav-active" exact>Users</NavLink>
+              <NavLink
+                to="/lazy3"
+                className={({ isActive }) => (isActive ? "nav-active" : "")}>
+                Lazy 3
+              </NavLink>
             </li>
           </ul>
         </nav>
 
-       
-       <Routes>
-         <Route path="/lazy1" element={} />
-       </Routes>
+        <Routes>
+          <Route path="/lazy1" element={<LazyPage1 />} />
+          <Route path="/lazy2" element={<LazyPage2 />} />
+          <Route path="/lazy3" element={<LazyPage3 />} />
+        </Routes>
       </div>
     </BrowserRouter>
   );
-}
+};
