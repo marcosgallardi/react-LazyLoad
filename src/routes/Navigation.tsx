6507b23ab792a16122b524446097640cf78394ea
marcosgallardi <gallardimarcos98@gmail.com>
2023-12-18 17:04:40 -0300

    avanzando

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,6 +1,6 @@
 import {
-  BrowserRouter as Router,
-  Switch,
+  BrowserRouter ,
+  Routes,
   Route,
   NavLink
 } from 'react-router-dom';
@@ -9,37 +9,28 @@ import logo from '../logo.svg';
 
 export const Navigation = () => {
   return (
-    <Router>
+    <BrowserRouter>
       <div className="main-layout">
         <nav>
             <img src={ logo } alt="React Logo" />
           <ul>
             <li>
-              <NavLink to="/" activeClassName="nav-active" exact>Home</NavLink>
+              <NavLink to="/lazy1" activeClassName="nav-active" exact>Home</NavLink>
             </li>
             <li>
-              <NavLink to="/about" activeClassName="nav-active" exact>About</NavLink>
+              <NavLink to="/lazy2" activeClassName="nav-active" exact>About</NavLink>
             </li>
             <li>
-              <NavLink to="/users" activeClassName="nav-active" exact>Users</NavLink>
+              <NavLink to="/lazy3" activeClassName="nav-active" exact>Users</NavLink>
             </li>
           </ul>
         </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about">
-            <h1>About</h1>
-          </Route>
-          <Route path="/users">
-            <h1>Users</h1>
-          </Route>
-          <Route path="/">
-            <h1>Home</h1>
-          </Route>
-        </Switch>
+       
+       <Routes>
+         <Route path="/lazy1" element={} />
+       </Routes>
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
