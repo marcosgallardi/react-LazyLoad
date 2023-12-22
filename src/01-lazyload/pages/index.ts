b73bef6af4c4cea506aa3992c8b19417fd8b9a61
marcosgallardi <gallardimarcos98@gmail.com>
2023-12-22 09:08:04 -0300

    rutas anidadas

---
+++ src/01-lazyload/pages/index.ts
@@ -0,0 +1,5 @@
+import LazyPage2 from "./LazyPage2";
+import LazyPage3 from "./LazyPage3";
+import LazyPage1 from "./LazyPage1";
+
+export { LazyPage2, LazyPage3, LazyPage1 };
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -9,27 +9,18 @@ interface Route {
   name: string;
 }
 
-const lazy1 = lazy(() => import(/* webpackChunkName:"LazyPage1" */"../01-lazyload/pages/LazyPage1"));
-const lazy2 = lazy(() => import(/* webpackChunkName:"LazyPage2" */"../01-lazyload/pages/LazyPage2"));
-const lazy3 = lazy(() => import(/* webpackChunkName:"LazyPage3" */"../01-lazyload/pages/LazyPage3"));
+const LazyLayout = lazy(
+  () =>
+    import(
+      /* webpackChunkName:"LazyLayout" */ "../01-lazyload/layout/LazyLayout"
+    )
+);
 
 export const routes: Route[] = [
   {
     to: "/lazy1",
-    path: "/lazy1",
-    Component: lazy1,
+    path: "/lazyload/*",
+    Component: LazyLayout,
     name: "lazy-1",
-  },
-  {
-    to: "/lazy2",
-    path: "/lazy2",
-    Component: lazy2,
-    name: "lazy-2",
-  },
-  {
-    to: "/lazy3",
-    path: "/lazy3",
-    Component: lazy3,
-    name: "lazy-3",
   },
 ];
