c49f35675cb7ca11a5604804f112cd7dbd6b4978
marcosgallardi <gallardimarcos98@gmail.com>
2024-01-11 10:38:46 -0300

    avanzando en el compound

--- src/02-Componets-Patterns/Components/ProductCard.tsx
+++ src/02-Componets-Patterns/Components/ProductCard.tsx
@@ -1,9 +1,11 @@
+import { ReactElement } from "react";
 import styles from "../Styles/styles.module.css";
 import noImage from "../Assets/no-image.jpg";
 import { useProduct } from "../Hooks/useProduct";
 
 interface Props {
   product: products;
+  children?: ReactElement | ReactElement[];
 }
 
 interface products {
@@ -12,26 +14,47 @@ interface products {
   img?: string;
 }
 
-export const ProductCard = ({ product }: Props) => {
-  const { count, handleCount } = useProduct();
-  return (
-    <div className={styles.productCard}>
-      <img
-        className={styles.productImg}
-        src={product.img ? product.img : noImage}
-        alt="coffee-image"
-      />
+interface ProductButtonsProps {
+  count: number;
+  handleCount: (value: number) => void;
+}
 
-      <span className={styles.productDescription}>{product.title}</span>
-      <div className={styles.buttonsContainer}>
-        <button className={styles.buttonMinus} onClick={() => handleCount(-1)}>
-          -
-        </button>
-        <div className={styles.countLabel}>{count}</div>
-        <button className={styles.buttonAdd} onClick={() => handleCount(+1)}>
-          +
-        </button>
-      </div>
+export const ProductImage = ({ img = "" }) => {
+  return (
+    <img className={styles.productImg} src={img ? img : noImage} alt="coffee" />
+  );
+};
+
+export const ProductTitle = ({ title }: { title: string }) => {
+  return <span className={styles.productDescription}>{title}</span>;
+};
+
+export const ProductButtons = ({ count, handleCount }: ProductButtonsProps) => {
+  return (
+    <div className={styles.buttonsContainer}>
+      <button className={styles.buttonMinus} onClick={() => handleCount(-1)}>
+        -
+      </button>
+      <div className={styles.countLabel}>{count}</div>
+      <button className={styles.buttonAdd} onClick={() => handleCount(+1)}>
+        +
+      </button>
     </div>
   );
 };
+
+export const ProductCard = ({ children, product }: Props) => {
+  const { count, handleCount } = useProduct();
+  return (
+    <div className={styles.productCard}>
+      {children}
+      {/* <ProductImage img={product.img} />
+      <ProductTitle title={product.title} />
+      <ProductButtons count={count} handleCount={handleCount} /> */}
+    </div>
+  );
+};
+
+ProductCard.Title = ProductTitle;
+ProductCard.Image = ProductImage;
+ProductCard.Buttons = ProductButtons;
--- src/02-Componets-Patterns/Pages/ShoppingPage.tsx
+++ src/02-Componets-Patterns/Pages/ShoppingPage.tsx
@@ -1,4 +1,9 @@
-import { ProductCard } from "../Components/ProductCard";
+import {
+  ProductCard,
+  ProductImage,
+  ProductTitle,
+  ProductButtons,
+} from "../Components/ProductCard";
 
 const product = {
   id: 1,
@@ -11,7 +16,27 @@ export const ShoppingPage = () => {
     <div>
       <h1>Shopping Store</h1>
       <hr />
-      <ProductCard product={product} />
+      <ProductCard product={product}>
+        <ProductCard.Image />
+        <ProductCard.Title title="Caffe" />
+        <ProductCard.Buttons
+          count={0}
+          handleCount={function (value: number): void {
+            throw new Error("Function not implemented.");
+          }}
+        />
+      </ProductCard>
+
+      {/* <ProductCard product={product}>
+        <ProductImage />
+        <ProductTitle title="Caffe" />
+        <ProductButtons
+          count={0}
+          handleCount={function (value: number): void {
+            throw new Error("Function not implemented.");
+          }}
+        />
+      </ProductCard> */}
     </div>
   );
 };
